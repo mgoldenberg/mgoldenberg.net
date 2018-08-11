a23420d0000c7ec947c12c063ccbe0676640c978
Michael Goldenberg <mgoldenberg6@gmail.com>
2018-08-11 21:15:05 +0000

    add pgp page

--- pgp/js/script.js
+++
@@ -1,6 +0,0 @@
-const colors = ["red", "orange", "gold", "green", "lightblue", "indigo", "violet"];
-let links = document.links;
-for(let i = 0; i < links.length; i++) {
-    links[i].style.color = colors[i % colors.length];
-}
-
